3b701623be6c0f42c50a9d1ad536da23a14f637b
mickaelBell <mickael.belleuvre@gmail.com>
2020-12-14 14:48:51 +0100

    first commit

---
+++ src/test/java/com/wcs/DoctorWho/DoctorWhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcs.DoctorWho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorWhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
